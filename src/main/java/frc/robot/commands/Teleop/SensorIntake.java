5b1bc0f8792838a1cd5d45119f11c4ab9b3f1c79
Bill Hong <hongw@salinehornets.org>
2024-12-07 16:17:35 -0500

    Added the SensorIntake command

---
+++ src/main/java/frc/robot/commands/Teleop/SensorIntake.java
@@ -0,0 +1,41 @@
+package frc.robot.commands.Teleop;
+
+import au.grapplerobotics.LaserCan;
+import au.grapplerobotics.LaserCan.Measurement;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.subsystems.IntakeSubsystem;
+
+// For moving the intake back during shooting
+public class SensorIntake extends Command {
+    private IntakeSubsystem intakeSubsystem;
+    private LaserCan laserCanSensor1;
+    private LaserCan laserCanSensor2;
+
+    public SensorIntake(IntakeSubsystem intake, LaserCan lcs1, LaserCan lcs2) {
+        intakeSubsystem = intake;
+        laserCanSensor1 = lcs1;
+        laserCanSensor2 = lcs2;
+        addRequirements(intakeSubsystem);
+    }
+
+    public void execute() {
+        double measurement1 = laserCanSensor1.getMeasurement().distance_mm;
+        
+        if (measurement1 <= Constants.LaserCan.INTAKE_WIDTH_MM - Constants.LaserCan.INTAKE_TOLERANCE_MM_1
+        && measurement1 != -1) {
+            intakeSubsystem.setIntakeSpeed(Constants.Speed.INTAKE/5);
+        }
+        else {
+            intakeSubsystem.setIntakeSpeed(Constants.Speed.INTAKE);
+        }
+    }
+
+    public boolean isFinished() {
+        double measurement2 = laserCanSensor2.getMeasurement().distance_mm;
+
+        return (measurement2 <= Constants.LaserCan.INTAKE_WIDTH_MM - Constants.LaserCan.INTAKE_TOLERANCE_MM_2
+         && measurement2 != -1);
+    }
+}
